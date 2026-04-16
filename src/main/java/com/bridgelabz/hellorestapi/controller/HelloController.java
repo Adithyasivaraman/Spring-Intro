6a259d5e79c30ea6560735317e31f1516b649ea3
Adithya S <adithyasivaraman5@gmail.com>
2026-04-16 11:52:23 +0530

    [Adithya] Added : UC4 : POST API with User DTO

--- src/main/java/com/bridgelabz/hellorestapi/controller/HelloController.java
+++ src/main/java/com/bridgelabz/hellorestapi/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.hellorestapi.controller;
 
+import com.bridgelabz.hellorestapi.dto.UserDTO;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,5 +19,10 @@ public class HelloController {
     @GetMapping("/hello/param/{name}")
     public String sayHelloWithPath(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
+    }
+
+    @PostMapping("/hello/post")
+    public String sayHelloPost(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
     }
 }
